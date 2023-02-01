be24f42e22ed86f7d3a6d61c7287b7fd4047a93a
matthewcuan <cuanmatthewl@gmail.com>
2023-01-31 16:10:58 -0800

    modified movie.js and movie.html to include cards for reviews

--- fcc_tutorial/frontend_copy/movie.js
+++ fcc_tutorial/frontend_copy/movie.js
@@ -2,56 +2,32 @@ const url = new URL(location.href);
 const movieId = url.searchParams.get("id"); 
 const movieTitle = url.searchParams.get("title")
 
-const APILINK = 'http://localhost:8000/api/v1/reviews/' + movieId;
+const APILINK = 'http://localhost:8000/api/v1/reviews/';
 
 const main = document.getElementById("section");
 const title = document.getElementById("title");
 
 title.innerText = movieTitle;
 
-returnMovies(APILINK);
-function returnMovies(url) {
-    fetch(url).then(res => res.json())
+returnReviews(APILINK);
+function returnReviews(url) {
+    fetch(url + "movie/" + movieId).then(res => res.json())
     .then(function(data) {
-        console.log(data.results);
-        data.results.forEach(element => {
+        console.log(data);
+        data.forEach(review => {
             const div_card = document.createElement('div');
-            div_card.setAttribute('class', 'card');
-
-            const div_row = document.createElement('div');
-            div_row.setAttribute('class', 'row');
-
-            const div_column = document.createElement('div');
-            div_column.setAttribute('class', 'column');
-
-            const image = document.createElement('img');
-            image.setAttribute('class', 'thumbnail');
-            image.setAttribute('id', 'image');
-
-            const title = document.createElement('h3');
-            title.setAttribute('id', 'title');
-
-            const center = document.createElement('div');
-            center.setAttribute('class', 'center');
-
-            const link = document.createElement('a');
-            link.setAttribute('class', 'link')
-
-            // const release_date = document.createElement('p');
-            // release_date.setAttribute('class', )
-            
-            movie_title = `${element.title}`;
-            title.innerHTML = movie_title;
-            link.href = SEARCH_PATH + movie_title.split(" ").join("+");
-            link.target = "_blank";
-            image.src = IMG_PATH + element.poster_path;
-
-            center.appendChild(image);
-            div_card.appendChild(center);
-            div_card.appendChild(title);
-            link.appendChild(div_card);
-            div_column.appendChild(link);
-            div_row.appendChild(div_column);
+            div_card.innerHTML = `
+            <div class="row">
+                <div class="column">
+                    <div class="card" id="${review._id}">
+                        <p><strong>Review: </strong>${review.review}</p>
+                        <p><strong>User: </strong>${review.user}</p>
+                        <p><a href="#" onclick="editReview('${review._id}', '${review.review}', 
+                        '${review.user}')">✏</a> <a href="#" onclick="deleteReview"('${review._id}')">🗑️</a></p>
+                    </div>
+                </div>
+            </div>
+            `
 
             main.appendChild(div_row);
         });
