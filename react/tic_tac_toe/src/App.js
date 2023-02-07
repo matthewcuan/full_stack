3fef070cf2d5ac18350f316767d90e3f6fe0b6be
matthewcuan <cuanmatthewl@gmail.com>
2023-02-07 11:49:14 -0800

    added draw notifications, now highlights board grey on draw

--- react/tic_tac_toe/src/App.js
+++ react/tic_tac_toe/src/App.js
@@ -1,15 +1,16 @@
 
 import { useState } from 'react';
 
-function Square({value, onSquareClick}) {
+function Square({ value, onSquareClick, isWinningSquare, onDraw }) {
   return (
-    <button className="square" onClick={onSquareClick}>
+    <button className={`square ${isWinningSquare ? "winning-square": ""} ${onDraw ? "draw": ""}`} 
+    onClick={onSquareClick}>
       {value}
     </button>
   );
 }
 
-function Board({ xIsNext, squares, onPlay }) {
+function Board({ xIsNext, squares, onPlay, winningSquares, gameOver }) {
   function handleClick(i) {
     if (squares[i] || calculateWinner(squares)) {
       return
@@ -26,9 +27,11 @@ function Board({ xIsNext, squares, onPlay }) {
   const winner = calculateWinner(squares);
   let status;    
   if (winner) {
-    status = "Winner: " + winner;
-  } else {
+    status = "Winner: " + winner[0];
+  }  else if (!gameOver) {
     status = "Next player: " + (xIsNext ? "X" : "O");
+  } else {
+    status = "It's a draw!"
   }
 
   function createGrid() {
@@ -36,7 +39,15 @@ function Board({ xIsNext, squares, onPlay }) {
     for (let i = 0; i < 3; i++) {
       let row = [];
       for (let j = 0; j < 3; j++) {
-        row.push(<Square value={squares[(j * 3) + i]} onSquareClick={() => handleClick((j * 3) + i)} />); 
+        const squareIndex = (j * 3) + i;
+        row.push(
+        <Square 
+          key={squareIndex}
+          value={squares[squareIndex]} 
+          onSquareClick={() => handleClick(squareIndex)} 
+          onDraw={gameOver && !winner}
+          isWinningSquare={winningSquares && winningSquares.includes(squareIndex)}/>
+        ); 
       }
       grid.push(<div className="board-row">{row}</div>);
     }
@@ -63,31 +74,52 @@ export default function Game() {
     setCurrentMove(nextHistory.length - 1);
   }
 
+  const winner = calculateWinner(currentSquares);
+  const winningSquares = winner ? winner.slice(1) : null;
+
+  const gameOver = currentMove === 9;
+
   function jumpTo(nextMove) {
     setCurrentMove(nextMove);
   }
 
   const moves = history.map((squares, move) => {
-    let description;
-    if (move > 0) {
-      description = 'Go to move #' + move;
-    } else {
-      description = 'Go to game start';
-    }
-    return (
-      <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
+    if (move !== currentMove) {
+      let description;
+      (move > 0) ? description = 'Go to move #' + move: 
+        description = 'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => jumpTo(move)}>{description}</button>
+        </li>
+      );
+    } else if (!gameOver) {
+      return (
+        <li key={move}>
+        You are at move #{currentMove}.
       </li>
-    );
+      )
+    } else {
+      return (
+        <li key={move}>
+        Game over!
+      </li>
+      )
+    }
   })
 
   return (
     <div className="game">
       <div className="game-board">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+        <Board 
+          xIsNext={xIsNext} 
+          squares={currentSquares} 
+          onPlay={handlePlay} 
+          winningSquares={winningSquares}
+          gameOver={gameOver}
+          />
       </div>
       <div className="game-info">
-        You are at move #{currentMove}.
         <ol start="0">{moves}</ol>
       </div>
     </div>
@@ -108,7 +140,7 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return [squares[a], a, b, c];
     }
   }
   return null;
