4b4d5540bd91224b26b8e965054d24796aff2522
matthewcuan <cuanmatthewl@gmail.com>
2023-02-10 15:19:25 -0800

    attempting to implement game logic into board

--- chess/src/components/game.js
+++ chess/src/components/game.js
@@ -1,24 +1,47 @@
-import { Chess} from 'chess.js';
-import { Chessboard } from "react-chessboard";
-
-var randomBoard = null
-var game = new Chess()
-
-function makeRandomMove () {
-  var possibleMoves = game.moves()
-
-  // exit if the game is over
-  if (game.game_over()) return
-
-  var randomIdx = Math.floor(Math.random() * possibleMoves.length)
-  game.move(possibleMoves[randomIdx])
-  randomBoard.position(game.fen())
-
-  window.setTimeout(makeRandomMove, 500)
+import { useState } from 'react';
+import { Chessboard } from 'react-chessboard';
+import Chess from 'chess.js';
+function App() {
+  const [game, setGame] = useState(new Chess());
+  
+  // perform modify function on game state
+  function safeGameMutate(modify) {
+    setGame((g) => {
+      const update = { ...g };
+      modify(update);
+      return update;
+    });
+  }
+  // make computer move
+  function makeRandomMove() {
+    const possibleMoves = game.moves();
+    // exit if the game is over
+    if (game.game_over() || game.in_draw() || possibleMoves.length === 0) return;
+    // select random move
+    const randomIndex = Math.floor(Math.random() * possibleMoves.length);
+    // play random move
+    safeGameMutate((game) => {
+      game.move(possibleMoves[randomIndex]);
+    });
+  }
+  // perform action when piece dropped by user
+  function onDrop(sourceSquare, targetSquare) {
+    // attempt move
+    let move = null;
+    safeGameMutate((game) => {
+      move = game.move({
+        from: sourceSquare,
+        to: targetSquare,
+        promotion: 'q'
+      });
+    });
+    
+    // illegal move made
+    if (move === null) return false;
+    // valid move made, make computer move
+    setTimeout(makeRandomMove, 200);
+    return true;
+  }
+  return <Chessboard position={game.fen()} onPieceDrop={onDrop} />;
 }
-
-randomBoard = Chessboard('myBoard', 'start')
-
-window.setTimeout(makeRandomMove, 500)
-
-export default randomBoard
+export default App;
