4307471061b3aae0c0379a70aa8be8f99757f518
matthewcuan <cuanmatthewl@gmail.com>
2023-02-13 15:08:46 -0800

    attempting to integrate chess.js into react-chessboard

--- chess/src/App.js
+++ chess/src/App.js
@@ -1,11 +1,62 @@
-import Game from "./components/game.js"
 import "./App.css"
+import { React } from "react";
+import { Chessboard } from "react-chessboard";
+import { Chess } from "chess.js";
 
 export default function App() {
 
+  const game = new Chess();
+
+  function onDragStart (source, piece, position, orientation) {
+    console.log("dragging")
+    // do not pick up pieces if the game is over
+    if (game.isGameOver()) return false
+  
+    // only pick up pieces for the side to move
+    if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+        (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+      return false
+    }
+  }
+
+  function onDrop (source, target) {
+    console.log("dropping")
+    // see if the move is legal
+    var move = game.move({
+      from: source,
+      to: target,
+      promotion: 'q' // NOTE: always promote to a queen for example simplicity
+    })
+  
+    // illegal move
+    if (move === null) {
+      alert("Illegal move! Please try again.")
+      return 'snapback'
+    }
+  
+  }
+
+  function newBoard(fen) {
+    const config = {
+      draggable: true,
+      position: game.fen(),
+      onDragStart: onDragStart,
+      onDrop: onDrop,
+      // onSnapEnd: onSnapEnd
+    }
+    return (
+      <Chessboard
+        config={config}
+      />
+    )
+  }
+ 
   return (
-    <div class="main">
-      <Game />
+    <div className="main">
+      <div className="board">
+      {newBoard(game.fen())}
+      </div>
     </div>
-  )
+  ) 
+
 }
--- chess/src/components/game.js
+++ chess/src/components/game.js
@@ -1,12 +1,67 @@
 import { React } from "react";
 import { Chessboard } from "react-chessboard";
+import { Chess } from "chess.js";
 
 export default function Game() {
   
+  // const board = null;
+  const game = new Chess();
+  // var [status, setStatus] = game.status();
+  // var [fen, setFen] = game.fen();
+  // var [pgn,setPgn] = game.pgn();
+
+  function handleDragStart (source, piece, position, orientation) {
+    console.log("dragging")
+    // do not pick up pieces if the game is over
+    if (game.game_over()) return false
+  
+    // only pick up pieces for the side to move
+    if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+        (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+      return false
+    }
+  }
+
+  function handleDrop (source, target) {
+    console.log("dropping")
+    // see if the move is legal
+    var move = game.move({
+      from: source,
+      to: target,
+      promotion: 'q' // NOTE: always promote to a queen for example simplicity
+    })
+  
+    // illegal move
+    if (move === null) return 'snapback'
+  
+  }
+
+  // var config = {
+  //   draggable: true,
+  //   position: 'start',
+  //   onDragStart: onDragStart,
+  //   onDrop: onDrop,
+  //   onSnapEnd: onSnapEnd
+  // }
+
+  // board = Chessboard('myBoard', config)
+
   return (
-    <div class="board">
-      <Chessboard />
+    <div className="board">
+      <Chessboard
+        draggable='true'
+        position='start'
+        onDragStart={handleDragStart}
+        onDrop={handleDrop}
+      />
+      {/* <label>Status:</label>
+      <div id="status"></div>
+      <label>FEN:</label>
+      <div id="fen"></div>
+      <label>PGN:</label>
+      <div id="pgn"></div> */}
     </div>
+    
   ) 
 
 }
