b59b1ad1e5fc70ada73fbe9dfdcc18eb20afd7e9
matthewcuan <cuanmatthewl@gmail.com>
2023-02-15 11:30:23 -0800

    got pieces to move in game when moved on board (need to fix: pieces moving on board)

--- chess/src/App.js
+++ chess/src/App.js
@@ -1,40 +1,45 @@
 import "./App.css"
 import { React } from "react";
 import { Chessboard } from "react-chessboard";
-// import { Chess } from "chess.js";
+import { Chess } from "chess.js";
 
 export default function App() {
 
-  // const game = new Chess();
+  const game = new Chess();
+  console.log(game.ascii());
 
-  // function onDragStart (source, piece, position, orientation) {
-  //   console.log("dragging")
-  //   // do not pick up pieces if the game is over
-  //   if (game.isGameOver()) return false
+  function handlePieceDragBegin (source, piece, position, orientation) {
+    console.log("dragging")
+    // do not pick up pieces if the game is over
+    if (game.isGameOver()) return false
   
-  //   // only pick up pieces for the side to move
-  //   if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
-  //       (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
-  //     return false
-  //   }
-  // }
+    // only pick up pieces for the side to move
+    if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+        (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+      return false
+    }
+  }
 
-  // function onDrop (source, target) {
-  //   console.log("dropping")
-  //   // see if the move is legal
-  //   var move = game.move({
-  //     from: source,
-  //     to: target,
-  //     promotion: 'q' // NOTE: always promote to a queen for example simplicity
-  //   })
+  function handlePieceDrop (source, target) {
+    console.log("dropped")
+    // see if the move is legal
+    var move = game.move({
+      from: source,
+      to: target,
+      promotion: 'q' // NOTE: always promote to a queen for example simplicity
+    })
   
-  //   // illegal move
-  //   if (move === null) {
-  //     alert("Illegal move! Please try again.")
-  //     return 'snapback'
-  //   }
-  
-  // }
+    // illegal move
+    if (move === null) {
+      alert("Illegal move! Please try again.")
+      return 'snapback'
+    }
+    
+    console.log(game.ascii());
+    console.log("should return true")
+
+    return true;
+  }
 
   // function newBoard(fen) {
   //   const config = {
@@ -50,17 +55,16 @@ export default function App() {
   //     />
   //   )
   // }
- 
+
   return (
     <div className="main">
       <div className="board">
         <Chessboard
-          onDragStart={() => console.log('You dragged me!')}
-          onDrop={() => console.log('You dropped me >:(')}
+          arePiecesDraggable={true}
+          onPieceDragBegin={handlePieceDragBegin}
+          onPieceDrop={handlePieceDrop}        
         />
       </div>
     </div>
   ) 
-
-
 } 
