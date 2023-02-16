e2a5c5c33a8cb15860be92c030d6083977d60ed6
matthewcuan <cuanmatthewl@gmail.com>
2023-02-16 14:37:05 -0800

    rewrote handlePieceDrop, working on setHistory

--- chess/src/App.js
+++ chess/src/App.js
@@ -6,26 +6,48 @@ import { Chess } from "chess.js";
 export default function App() {
 
   const [game, setGame] = useState(new Chess());
-  const [history, setHistory] = useState(game.history());
-  console.log("at the top")
-  console.log(game.ascii());
-
-  function safeGameMutate(modify){
-    setGame(()=> {
-      console.log("updating game")
-      const update = new Chess ()
-      console.log("attempting to load old fen to new game")
-      console.log(game.ascii())
-      update.load(game.fen())
-      console.log("loaded old fen to new game")
-      console.log(update.ascii())
-      console.log("attempting to update to new fen")
-      modify(update)
-      console.log("updated to new fen")
-      console.log(update.ascii())
-      return update
+  const [history, setHistory] = useState([game.history()]);
+  
+  function handlePieceDrop(source, target) {
+    let move = game.move({
+      from: source,
+      to: target,
+      promotion: 'q'
     })
-  }
+
+    if (move) {
+      setGame(game);
+      console.log(game.ascii());
+      setHistory(game.history());
+      console.log(history)
+      return true;
+    }
+
+    alert("Illegal move! Try again.");
+    return false;
+  };
+ 
+  // const [game, setGame] = useState(new Chess());
+  // const [history, setHistory] = useState(game.history());
+  // console.log("at the top")
+  // console.log(game.ascii());
+
+  // function safeGameMutate(modify){
+  //   setGame(()=> {
+  //     console.log("updating game")
+  //     const update = new Chess ()
+  //     console.log("attempting to load old fen to new game")
+  //     console.log(game.ascii())
+  //     update.load(game.fen())
+  //     console.log("loaded old fen to new game")
+  //     console.log(update.ascii())
+  //     console.log("attempting to update to new fen")
+  //     modify(update)
+  //     console.log("updated to new fen")
+  //     console.log(update.ascii())
+  //     return update
+  //   })
+  // }
 
   // function handlePieceDragBegin (source, piece, position, orientation) {
   //   console.log("dragging")
@@ -39,46 +61,46 @@ export default function App() {
   //   }
   // }
 
-  function handlePieceDrop (source, target, piece) {
-    console.log("dropped")
-    // see if the move is legal
-    let move = null;
-    console.log("going to update")
-    safeGameMutate((game) => {
-      console.log("modifying new game")
-      move = game.move({
-        from: source,
-        to: target,
-        promotion:'q'
-      })
-    })
+  // function handlePieceDrop (source, target, piece) {
+  //   console.log("dropped")
+  //   // see if the move is legal
+  //   let move = null;
+  //   console.log("going to update")
+  //   safeGameMutate((game) => {
+  //     console.log("modifying new game")
+  //     move = game.move({
+  //       from: source,
+  //       to: target,
+  //       promotion:'q'
+  //     })
+  //   })
     
-    console.log("updated")
-    console.log(move)
+  //   console.log("updated")
+  //   console.log(move)
   
-    // illegal move
-    if (move === null) {
-      // alert("Illegal move! Please try again.")
-      console.log("illegal move")
-      return false;
-    }
+  //   // illegal move
+  //   if (move === null) {
+  //     // alert("Illegal move! Please try again.")
+  //     console.log("illegal move")
+  //     return false;
+  //   }
     
-    console.log("after move")  
-    console.log(game.ascii())
-    console.log("finishing function")
+  //   console.log("after move")  
+  //   console.log(game.ascii())
+  //   console.log("finishing function")
 
-    return true;
-  }
+  //   return true;
+  // }
 
-  function fen() {
-    return game.fen();
-  }
+  // function fen() {
+  //   return game.fen();
+  // }
 
   return (
     <div className="main">
       <div className="board">
         <Chessboard 
-          position={fen()}
+          position={game.fen()}
           // onPieceDragBegin={handlePieceDragBegin}
           onPieceDrop={handlePieceDrop}
         />
