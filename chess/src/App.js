d69b35ea113ead7dad634d4c13071e0fd8d5fd2f
matthewcuan <cuanmatthewl@gmail.com>
2023-02-16 12:32:32 -0800

    attempting to debug

--- chess/src/App.js
+++ chess/src/App.js
@@ -6,71 +6,69 @@ import { Chess } from "chess.js";
 export default function App() {
 
   const [game, setGame] = useState(new Chess());
+  const [history, setHistory] = useState(game.history());
   console.log("at the top")
   console.log(game.ascii());
 
   function safeGameMutate(modify){
     setGame(()=> {
+      console.log("updating game")
       const update = new Chess ()
-      update.loadPgn(game.pgn())
+      console.log("attempting to load old fen to new game")
+      console.log(game.ascii())
+      update.load(game.fen())
+      console.log("loaded old fen to new game")
+      console.log(update.ascii())
+      console.log("attempting to update to new fen")
       modify(update)
+      console.log("updated to new fen")
+      console.log(update.ascii())
       return update
     })
   }
 
-  function handlePieceDragBegin (source, piece, position, orientation) {
-    console.log("dragging")
-    // do not pick up pieces if the game is over
-    if (game.isGameOver()) return false
+  // function handlePieceDragBegin (source, piece, position, orientation) {
+  //   console.log("dragging")
+  //   // do not pick up pieces if the game is over
+  //   if (game.isGameOver()) return false
   
-    // only pick up pieces for the side to move
-    if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
-        (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
-      return false
-    }
-  }
+  //   // only pick up pieces for the side to move
+  //   if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
+  //       (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+  //     return false
+  //   }
+  // }
 
   function handlePieceDrop (source, target, piece) {
     console.log("dropped")
     // see if the move is legal
     let move = null;
+    console.log("going to update")
     safeGameMutate((game) => {
+      console.log("modifying new game")
       move = game.move({
         from: source,
         to: target,
         promotion:'q'
       })
-  })
-
-  console.log(move)
+    })
+    
+    console.log("updated")
+    console.log(move)
   
     // illegal move
     if (move === null) {
-      alert("Illegal move! Please try again.")
+      // alert("Illegal move! Please try again.")
+      console.log("illegal move")
       return false;
     }
     
     console.log("after move")  
-    console.log(fen())
+    console.log(game.ascii())
     console.log("finishing function")
 
     return true;
   }
-
-  // function newBoard(fen) {
-  //   const config = {
-  //     draggable: true,
-  //     position: game.fen(),
-  //     onDragStart: onDragStart,
-  //     onDrop: onDrop,
-  //     // onSnapEnd: onSnapEnd
-  //   }
-  //   return (
-  //     <Chessboard
-  //       config={config}
-  //     />
-  //   )
-  // }
 
   function fen() {
     return game.fen();
@@ -81,7 +79,7 @@ export default function App() {
       <div className="board">
         <Chessboard 
           position={fen()}
-          onPieceDragBegin={handlePieceDragBegin}
+          // onPieceDragBegin={handlePieceDragBegin}
           onPieceDrop={handlePieceDrop}
         />
       </div>
