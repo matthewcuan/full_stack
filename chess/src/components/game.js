7fa8afdcdff96a70e7f277bf4da8825faf2ecf2a
matthewcuan <cuanmatthewl@gmail.com>
2023-02-17 12:13:26 -0800

    moved game logic to game.js component

--- chess/src/components/game.js
+++ chess/src/components/game.js
@@ -1,42 +1,42 @@
+import { React, useState } from "react";
+import { Chessboard } from "react-chessboard"
 import { Chess } from "chess.js";
 
 export default function Game() {
   
-  const game = new Chess();
-  console.log(game.ascii());
-
-  function handlePieceDragBegin (source, piece, position, orientation) {
-    console.log("dragging")
-    // do not pick up pieces if the game is over
-    if (game.isGameOver()) return false
+  const [game, setGame] = useState(new Chess());
+  const [history, setHistory] = useState([game.fen()]);
   
-    // only pick up pieces for the side to move
-    if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
-        (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
-      return false
-    }
-  }
-
-  function handlePieceDrop (source, target) {
-    console.log("dropped")
-    // see if the move is legal
-    var move = game.move({
+  function handlePieceDrop(source, target) {
+    let move = game.move({
       from: source,
       to: target,
-      promotion: 'q' // NOTE: always promote to a queen for example simplicity
+      promotion: 'q'
     })
-  
-    // illegal move
-    if (move === null) {
-      alert("Illegal move! Please try again.")
-      return 'snapback';
+
+    // legal move
+    if (move) {
+      setGame(game);
+      console.log(game.ascii());
+      setHistory([...history, game.fen()]);
+      console.log(history)
+      return true;
     }
-    
-    console.log(game.ascii());
-    console.log(game.fen())
-    console.log("should be returning true")
-    
-    return true;
-  }
+
+    // illegal move
+    alert("Illegal move! Try again.");
+    return false;
+  };
+
+  return (
+    <div className="main">
+      <div className="board">
+        <Chessboard 
+          position={game.fen()}
+          onPieceDrop={handlePieceDrop}
+        />
+      </div>
+    </div>
+  ) 
 
 }
