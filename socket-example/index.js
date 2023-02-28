33594f4c1544750dca7fccc7c2d77abaa180b5f6
matthewcuan <cuanmatthewl@gmail.com>
2023-02-28 13:01:26 -0800

    socket chat tutorial

---
+++ socket-example/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+const port = 3030;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+
+});
+
+server.listen(port, () => {
+  console.log(`listening on *:${port}`);
+});
